4696632b5c425313dbbe53174cef76f010627822
Piyush Bansal <piyushbansal@Piyushs-MacBook-Air.local>
2024-03-25 15:54:47 +0530

    install reactand tailwind css and create first template

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './public/index.html',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
